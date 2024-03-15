d4c0dc86325d338728431d44824f7122baba8e20
Miguel Méndez Murias <miguelmendeztech@gmail.com>
2024-03-15 09:52:49 +0100

    SDI-2324-310-sesión8-ejercicio-complementario2

--- musicstoreapp/routes/authors.js
+++ musicstoreapp/routes/authors.js
@@ -1,6 +1,7 @@
+const {response} = require("express");
 module.exports = function(app) {
-    app.get("/authors", function(req, res) {
-        let authors=[{
+    app.get("/authors", function (req, res) {
+        let authors = [{
             "name": "author1",
             "group": "grupo1",
             "role": "rol1"
@@ -13,22 +14,32 @@ module.exports = function(app) {
             "group": "grupo3",
             "role": "rol3"
         }];
-        let response={
-            authors:authors
+        let response = {
+            authors: authors
         };
-        res.render("authors/authors.twig",response);
+        res.render("authors/authors.twig", response);
     });
     app.get('/authors/add', function (req, res) {
-        res.render("authors/add.twig");
+        let roles = [
+            { "name": "cantante"},
+            { "name": "trompetista"},
+            { "name": "violinista"},
+            { "name": "saxofonista"},
+            { "name": "pianista"}
+        ];
+        let response={
+            "roles":roles
+        };
+        res.render("authors/add.twig", response);
     });
-    app.post('/authors/add', function (req,res){
-        let nullParameter="no enviado en la petición"
-        let response="Autor agregado: "+(req.body.name||"name "+nullParameter)+"<br>"
-            + " grupo: "+(req.body.group||"group "+nullParameter)+"<br>"
-            + " rol: "+(req.body.role||"role "+nullParameter)
+    app.post('/authors/add', function (req, res) {
+        let nullParameter = "no enviado en la petición"
+        let response = "Autor agregado: " + (req.body.name || "name " + nullParameter) + "<br>"
+            + " grupo: " + (req.body.group || "group " + nullParameter) + "<br>"
+            + " rol: " + (req.body.role || "role " + nullParameter)
         res.send(response)
     });
-    app.get('/authors**', function (req,res){
+    app.get('/authors**', function (req, res) {
         res.redirect('/authors')
     });
 
