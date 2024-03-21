af452c27719675c36ed5c5b91ee9fa31bb338f06
Miguel Méndez Murias <miguelmendeztech@gmail.com>
2024-03-21 17:55:49 +0100

    SDI-2324-310-9.4-Registro de usuario, autenticación y uso de sesión.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -18,6 +18,10 @@ module.exports = function(app,songsRepository) {
         res.render("shop.twig",response);
     });
     app.get('/songs/add', function (req, res) {
+        if ( req.session.user == null){
+            res.redirect("/shop");
+            return;
+        }
         res.render("songs/add.twig");
     });
     app.get('/songs/:id', function (req, res) {
@@ -35,10 +39,15 @@ module.exports = function(app,songsRepository) {
         res.send(response);
     });
     app.post('/songs/add', function (req,res){
+        if ( req.session.user == null){
+            res.redirect("/shop");
+            return;
+        }
         let song = {
             title: req.body.title,
             kind: req.body.kind,
-            price: req.body.price
+            price: req.body.price,
+            author:req.session.user
         }
         songsRepository.insertSong(song, function(result){
             if (result.songId !== null && result.songId !== undefined) {
